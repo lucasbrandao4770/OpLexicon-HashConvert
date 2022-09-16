4c50e09d2e6916d9dd2cd9adabcd247a68d4395d
Lucas Brandão <brandao.brandao@discente.ufg.br>
2022-09-16 16:48:33 -0300

    Finished hasher class

--- hash_table.py
+++ hash_table.py
@@ -3,6 +3,3 @@ from hasher import Hasher
 class HashTable():
     size = 42209
     hasher = Hasher()
-
-
-    pass
--- hasher.py
+++ hasher.py
@@ -2,27 +2,19 @@ import hashlib
 
 
 class Hasher():
+    def __init__(self, table_size=42) -> None:
+        self.table_size = 42
 
     def hash(self, key, func_name):
         valid_names = ['md5', 'sha1', 'sha224', 'sha256', 'sha384']
-        if func_name in valid_names:
-            if func_name == 'md5':
-                return self.__md5(key)
-            else:
-                pass
-        pass
 
-    def __md5(self, key):
-        pass
+        assert func_name in valid_names, f"Please enter a valid hashing function. \
+                                            Valid functions are: {valid_names}\n"
 
-    def __sha1(self, key):
-        pass
+        hash_func = getattr(hashlib, f'{func_name}')
+        hashed_value = hash_func(key)
+        return self.__transform(hashed_value)
 
-    def __sha224(self, key):
-        pass
 
-    def __sha256(self, key):
-        pass
-
-    def __sha384(self, key):
-        pass
+    def __transform(self, key):
+        return (sum([ord(c) for c in key])**2) % self.table_size
