4b50965b7bb55d0575990c35f3956478e7e83f96
Lucas Brandão <brandao.brandao@discente.ufg.br>
2022-09-16 22:41:45 -0300

    Changed linear probing to pseudo-random probing

--- utils/hash_table.py
+++ utils/hash_table.py
@@ -1,42 +1,53 @@
 from utils.hasher import Hasher
+import numpy as np
 
 class HashTable():
-    __EMPTY = None
+    _EMPTY = None
     def __init__(self, size=42209, hashing_func='md5') -> None:
         self.size = size
         self.hasher = Hasher(table_size=size, hashing_func=hashing_func)
-        self.body = [self.__EMPTY]*size
+        self.body = [self._EMPTY]*size
         self.n_keys = 0
+        self._probe_seq = np.random.RandomState(seed=42).permutation(self.size)
+        self._collision_idx = -1
 
 
     def add(self, key, value):
         idx = self.hasher.hash(key)
 
-        while (self.body[idx] != HashTable.__EMPTY):
+        while (self.body[idx] != HashTable._EMPTY):
             if self.body[idx] == value:
                 return
-            idx = self.__handle_collision(idx)
+            idx = self._handle_collision(idx)
 
+        self._reset_collision_idx()
         self.body[idx] = value
         self.n_keys += 1
 
 
     def delete(self, key):
         idx = self.hasher.hash(key)
-        self.body[idx] = HashTable.__EMPTY
-
-
-    def __setitem__(self, key, value):
-        self.add(key, value)
+        self.body[idx] = HashTable._EMPTY
 
 
     def get(self, key, value):
         idx = self.hasher.hash(key)
-        while self.body[idx]!=value and self.body[idx]!=HashTable.__EMPTY:
-            idx = self.__handle_collision(idx)
 
+        while self.body[idx]!=value and self.body[idx]!=HashTable._EMPTY:
+            idx = self._handle_collision(idx)
+
+        self._reset_collision_idx()
         return self.body[idx]
 
 
-    def __handle_collision(self, idx):
-        return (idx+1) % self.size
+    def _handle_collision(self, idx):
+        self._collision_idx += 1
+        return (idx + self._probe_seq[self._collision_idx]) % self.size
+
+
+    def _reset_collision_idx(self):
+        self._collision_idx = -1
+
+
+    def __setitem__(self, key, value):
+        self.add(key, value)
